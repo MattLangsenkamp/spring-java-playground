e86ec6e641f888500dfe37796783d748df3c5787
matt langsenkamp <mattlangsenkamp@gmail.com>
2024-01-30 13:14:55 -0500

    model and controllers

--- src/main/java/com/mattlangsenkamp/learningspring/controller/ContentController.java
+++ src/main/java/com/mattlangsenkamp/learningspring/controller/ContentController.java
@@ -1,10 +1,54 @@
 package com.mattlangsenkamp.learningspring.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.mattlangsenkamp.learningspring.model.Content;
+import com.mattlangsenkamp.learningspring.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");
+        }
+        repository.save(content);
+    }
 }
--- src/main/java/com/mattlangsenkamp/learningspring/repository/ContentCollectionRepository.java
+++ src/main/java/com/mattlangsenkamp/learningspring/repository/ContentCollectionRepository.java
@@ -1,7 +1,13 @@
 package com.mattlangsenkamp.learningspring.repository;
 
 import com.mattlangsenkamp.learningspring.model.*;
+
+import jakarta.annotation.PostConstruct;
+
 import java.util.List;
+import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Repository;
@@ -9,6 +15,41 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>()
+    private final List<Content> contentList = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+
+    }
+
+    public List<Content> findAll() {
+        return contentList;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.removeIf(c -> c.id().equals(content.id()));
+        contentList.add(content);
+    }
+
+    public boolean existsById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).findAny().isPresent();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(
+                1,
+                "null",
+                "cool desc",
+                Status.IN_PROGRESS,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                "wow");
+        contentList.add(c);
+    }
 
 }
